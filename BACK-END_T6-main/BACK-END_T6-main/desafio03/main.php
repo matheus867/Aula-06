5ea07d916ee882f80606cd58b9d743d2045fb2ec
matheus867 <matheus20078119@gmail.com>
2025-08-19 21:55:52 -0300

    Add files via upload

---
+++ BACK-END_T6-main/BACK-END_T6-main/desafio03/main.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Desafio com Include</title>
+</head>
+
+<body>
+    <?php include 'functions.php'; ?>
+    <h2>Desafio com Include</h2>
+    <?php
+
+    //Apenas atribuir valores às variáveis
+    $num1 = 1;
+    $num2 = 9;
+    echo "<p>A soma de $num1 e $num2 é: " . calcularSoma($num1, $num2) . "</p>";
+
+    ?>
+</body>
+
+</html>
